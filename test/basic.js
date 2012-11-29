d9edd1ff397a22ceab3395d23276359e3142e732
isaacs <i@izs.me>
2012-11-28 18:23:04 -0800

    test

---
+++ test/basic.js
@@ -0,0 +1,96 @@
+var tt = require('../')
+
+if (process.argv[2] === 'child')
+  tt('child', child)
+else
+  tt('parent', parent)
+
+function child(t) {
+  // this passes
+  t.ok(true, 'yoyoyo')
+  t.ok(false, 'message')
+  t.equal(true, 1, 'basically the same')
+  t.strictEqual(true, 1, 'but not quite')
+  t.test('firstborn', function (t) {
+    t.deepEqual([1,2,3], [1,2,3])
+    t.test('grandchild', function (t) {
+      t.ok(true)
+      t.ok(false)
+      t.end()
+    })
+    t.test('grandchild 2', function (t) {
+      t.ok(true)
+      t.ok(false)
+      t.end()
+    })
+    t.end()
+  })
+  t.test('secondborn', function (t) {
+    t.deepEqual([1,2,3], [1,2,5], 'not the same!')
+    t.end()
+  })
+  t.end()
+}
+
+var expect = (function(){/*
+# child
+
+ok 1 ok
+
+not ok 2 AssertionError: message
+# at child (/Users/isaacs/dev/js/tt/test/basic.js:11:5)
+
+ok 3 equal
+
+not ok 4 AssertionError: but not quite
+# at child (/Users/isaacs/dev/js/tt/test/basic.js:13:5)
+
+# firstborn
+
+ok 5 deepEqual
+
+# grandchild
+
+ok 6 ok
+
+not ok 7 AssertionError: false == true
+# at /Users/isaacs/dev/js/tt/test/basic.js:18:9
+
+# grandchild 2
+
+ok 8 ok
+
+not ok 9 AssertionError: false == true
+# at /Users/isaacs/dev/js/tt/test/basic.js:23:9
+
+# secondborn
+
+not ok 10 AssertionError: not the same!
+# at /Users/isaacs/dev/js/tt/test/basic.js:29:7
+
+0..10
+
+# pass 5/10
+# fail 5/10
+*/}).toString().split(/\n/).slice(1, -1).join('\n')
+
+function parent(t) {
+  var spawn = require('child_process').spawn
+  var args = [ __filename, 'child' ]
+  var node = process.execPath
+  var child = spawn(node, args)
+  var stdout = ''
+  var stderr = ''
+  child.stdout.on('data', function(c) {
+    stdout += c
+  })
+  child.stderr.on('data', function(c) {
+    stderr += c
+  })
+  child.on('close', function(code, signal) {
+    t.equal(stdout.trim(), expect.trim(), 'correct output')
+    t.equal(code, 5, 'error code')
+    t.equal(signal, null, 'no signal')
+    t.end()
+  })
+}
